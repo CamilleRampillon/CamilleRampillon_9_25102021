c5c2e5aa3bd92df4dace9f4bd37a392969212bad
CamilleRampillon <camille.rampillon@gmail.com>
2021-11-02 11:15:12 +0100

    fix bug Bills

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -41,13 +41,13 @@ export default class {
             try {
               return {
                 ...doc.data(),
-                date: formatDate(doc.data().date),
+                date: doc.data().date,
                 status: formatStatus(doc.data().status)
               }
             } catch(e) {
               // if for some reason, corrupted data was introduced, we manage here failing formatDate function
               // log the error and return unformatted date in that case
-              console.log(e,'for',doc.data())
+              //console.log(e,'for',doc.data())
               return {
                 ...doc.data(),
                 date: doc.data().date,
